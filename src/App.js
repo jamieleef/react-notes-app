31c2f378150f54e068e9cdf811719bbc62c90c02
Jamie Feliu <jamieleefeliu@gmail.com>
2023-05-28 12:36:33 -0400

    State & Map Array

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList";
 
 const App = () => {
+    const [notes, setNotes] = useState([
+        {
+            id: nanoid(),
+            text: "This is my first note!",
+            date: "15/04/2021"
+        },
+        {
+            id: nanoid(),
+            text: "This is my second note!",
+            date: "21/04/2021"
+        },
+        {
+            id: nanoid(),
+            text: "This is my third note!",
+            date: "28/04/2023"
+        },
+        {
+            id: nanoid(),
+            text: "This is my new note!",
+            date: "28/04/2023"
+        },
+]);
+
     return <div className="container">
-        <NotesList />
+        <NotesList notes={notes} />
     </div>;
 };
 
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,11 +1,11 @@
 import { MdDeleteForever } from 'react-icons/md';
 
-const Note = () => {
+const Note = ({ id, text, date }) => {
     return(
         <div className='note'>
-            <span>First note of the day, yay!</span>
+            <span>{text}</span>
             <div className="note-footer">
-                <small>15/11/2000</small>
+                <small>{date}</small>
                 <MdDeleteForever className='delete-icon' size='1.3em' />
             </div>
 
