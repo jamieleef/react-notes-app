132e8482863e21ccb7a6d6c57f7b2e8556a0e4cd
Jamie Feliu <jamieleefeliu@gmail.com>
2023-05-28 13:13:21 -0400

    Add New Notes component

--- src/App.js
+++ src/App.js
@@ -7,28 +7,41 @@ const App = () => {
         {
             id: nanoid(),
             text: "This is my first note!",
-            date: "15/04/2021"
+            date: "15/04/2021",
         },
         {
             id: nanoid(),
             text: "This is my second note!",
-            date: "21/04/2021"
+            date: "21/04/2021",
         },
         {
             id: nanoid(),
             text: "This is my third note!",
-            date: "28/04/2023"
+            date: "28/04/2023",
         },
         {
             id: nanoid(),
             text: "This is my new note!",
-            date: "28/04/2023"
+            date: "28/04/2023",
         },
-]);
+    ]);
 
-    return <div className="container">
-        <NotesList notes={notes} />
-    </div>;
+    const addNote = (text) => {
+        const date = new Date();
+        const newNote = {
+            id: nanoid(),
+            text: text,
+            date: date.toLocaleDateString()
+        };
+        const newNotes = [...notes, newNote];
+        setNotes(newNotes);
+    };
+
+    return (
+        <div className="container">
+            <NotesList notes={notes} handleAddNote={addNote} />
+        </div>
+    );
 };
 
 export default App;
